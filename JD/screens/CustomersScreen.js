e6fd6907edbe1d705bd93a7fd8291aa7349e2a32
i-am-ballu <49309867+i-am-ballu@users.noreply.github.com>
2019-12-01 16:16:14 +0530

    Update For Customer screen

--- JD/screens/CustomersScreen.js
+++ JD/screens/CustomersScreen.js
@@ -1,206 +1,180 @@
 import * as WebBrowser from "expo-web-browser";
 import React from "react";
-import {
-  Image,
-  Platform,
-  ScrollView,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View
-} from "react-native";
-
+import { StyleSheet, View, Text, ScrollView } from "react-native";
+import { TextInput, Searchbar, DataTable } from "react-native-paper";
 import { MonoText } from "../components/StyledText";
 
-export default function CustomersScreen() {
-  return (
-    <View style={styles.container}>
-      <ScrollView
-        style={styles.container}
-        contentContainerStyle={styles.contentContainer}
-      >
-        <View style={styles.welcomeContainer}>
-          <Image
-            source={
-              __DEV__
-                ? require("../assets/images/robot-dev.png")
-                : require("../assets/images/robot-prod.png")
-            }
-            style={styles.welcomeImage}
+export default class CustomersScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchBarText: ""
+    };
+  }
+  onChangeText(text) {
+    this.setState({ searchBarText: text });
+  }
+
+  render() {
+    var accounts = [
+      {
+        accNumber: "1",
+        accType: "D",
+        productCode: "1",
+        availBalance: "1000"
+      },
+      {
+        accNumber: "2",
+        accType: "D",
+        productCode: "2",
+        availBalance: "2000"
+      },
+      {
+        accNumber: "3",
+        accType: "D",
+        productCode: "3",
+        availBalance: "3000"
+      },
+      {
+        accNumber: "4",
+        accType: "D",
+        productCode: "4",
+        availBalance: "4000"
+      },
+      {
+        accNumber: "5",
+        accType: "D",
+        productCode: "5",
+        availBalance: "5000"
+      },
+      {
+        accNumber: "6",
+        accType: "D",
+        productCode: "6",
+        availBalance: "6000"
+      },
+      {
+        accNumber: "7",
+        accType: "D",
+        productCode: "7",
+        availBalance: "7000"
+      },
+      {
+        accNumber: "8",
+        accType: "D",
+        productCode: "8",
+        availBalance: "8000"
+      },
+      {
+        accNumber: "9",
+        accType: "D",
+        productCode: "9",
+        availBalance: "9000"
+      },
+      {
+        accNumber: "10",
+        accType: "D",
+        productCode: "10",
+        availBalance: "10000"
+      },
+      {
+        accNumber: "11",
+        accType: "D",
+        productCode: "11",
+        availBalance: "11000"
+      },
+      {
+        accNumber: "12",
+        accType: "D",
+        productCode: "12",
+        availBalance: "12000"
+      },
+      {
+        accNumber: "13",
+        accType: "D",
+        productCode: "13",
+        availBalance: "13000"
+      },
+      {
+        accNumber: "14",
+        accType: "D",
+        productCode: "14",
+        availBalance: "14000"
+      },
+      {
+        accNumber: "15",
+        accType: "D",
+        productCode: "15",
+        availBalance: "15000"
+      },
+      {
+        accNumber: "16",
+        accType: "D",
+        productCode: "16",
+        availBalance: "16000"
+      }
+    ];
+    return (
+      <View style={styles.container}>
+        <View>
+          <Searchbar
+            style={{
+              margin: 8
+            }}
+            Type="flat"
+            onChangeText={text => this.onChangeText(text)}
+            value={this.state.searchBarText}
+            placeholder="Search by orderId."
+            theme={{
+              colors: {
+                underlineColor: "transparent",
+                background: "transparent",
+                placeholder: "#3498db",
+                text: "#3498db"
+              }
+            }}
           />
         </View>
-
-        <View style={styles.getStartedContainer}>
-          <DevelopmentModeNotice />
-
-          <Text style={styles.getStartedText}>Get started by opening</Text>
-
-          <View
-            style={[styles.codeHighlightContainer, styles.homeScreenFilename]}
-          >
-            <MonoText>screens/HomeScreen.js</MonoText>
-          </View>
-
-          <Text style={styles.getStartedText}>
-            Change this text and your app will automatically reload.
-          </Text>
-        </View>
-
-        <View style={styles.helpContainer}>
-          <TouchableOpacity onPress={handleHelpPress} style={styles.helpLink}>
-            <Text style={styles.helpLinkText}>
-              Help, it didn’t automatically reload!
-            </Text>
-          </TouchableOpacity>
-        </View>
-      </ScrollView>
-
-      <View style={styles.tabBarInfoContainer}>
-        <Text style={styles.tabBarInfoText}>
-          This is a tab bar. You can edit it in:
-        </Text>
-
-        <View
-          style={[styles.codeHighlightContainer, styles.navigationFilename]}
-        >
-          <MonoText style={styles.codeHighlightText}>
-            navigation/MainTabNavigator.js
-          </MonoText>
-        </View>
+        <ScrollView>
+          <DataTable style={{ margin: 8 }}>
+            <DataTable.Header style={{ colors: "#3498db" }}>
+              <DataTable.Title>Account</DataTable.Title>
+              <DataTable.Title>Code</DataTable.Title>
+              <DataTable.Title>Balance</DataTable.Title>
+            </DataTable.Header>
+            {accounts.map(account => {
+              return (
+                <DataTable.Row
+                  key={account.accNumber} // you need a unique key per item
+                  onPress={() => {
+                    // added to illustrate how you can make the row take the onPress event and do something
+                    console.log(`selected account ${account.accNumber}`);
+                  }}
+                >
+                  <DataTable.Cell>{account.accNumber}</DataTable.Cell>
+                  <DataTable.Cell>{account.productCode}</DataTable.Cell>
+                  <DataTable.Cell>{account.availBalance}</DataTable.Cell>
+                </DataTable.Row>
+              );
+            })}
+          </DataTable>
+        </ScrollView>
       </View>
-    </View>
-  );
-}
-
-function DevelopmentModeNotice() {
-  if (__DEV__) {
-    const learnMoreButton = (
-      <Text onPress={handleLearnMorePress} style={styles.helpLinkText}>
-        Learn more
-      </Text>
-    );
-
-    return (
-      <Text style={styles.developmentModeText}>
-        Development mode is enabled: your app will be slower but you can use
-        useful development tools. {learnMoreButton}
-      </Text>
-    );
-  } else {
-    return (
-      <Text style={styles.developmentModeText}>
-        You are not in development mode: your app will run at full speed.
-      </Text>
     );
   }
 }
-
-function handleLearnMorePress() {
-  WebBrowser.openBrowserAsync(
-    "https://docs.expo.io/versions/latest/workflow/development-mode/"
-  );
-}
-
-function handleHelpPress() {
-  WebBrowser.openBrowserAsync(
-    "https://docs.expo.io/versions/latest/workflow/up-and-running/#cant-see-your-changes"
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff"
-  },
-  developmentModeText: {
-    marginBottom: 20,
-    color: "rgba(0,0,0,0.4)",
-    fontSize: 14,
-    lineHeight: 19,
-    textAlign: "center"
-  },
-  contentContainer: {
-    paddingTop: 30
-  },
-  welcomeContainer: {
-    alignItems: "center",
-    marginTop: 10,
-    marginBottom: 20
-  },
-  welcomeImage: {
-    width: 100,
-    height: 80,
-    resizeMode: "contain",
-    marginTop: 3,
-    marginLeft: -10
-  },
-  getStartedContainer: {
-    alignItems: "center",
-    marginHorizontal: 50
-  },
-  homeScreenFilename: {
-    marginVertical: 7
-  },
-  codeHighlightText: {
-    color: "rgba(96,100,109, 0.8)"
-  },
-  codeHighlightContainer: {
-    backgroundColor: "rgba(0,0,0,0.05)",
-    borderRadius: 3,
-    paddingHorizontal: 4
-  },
-  getStartedText: {
-    fontSize: 17,
-    color: "rgba(96,100,109, 1)",
-    lineHeight: 24,
-    textAlign: "center"
-  },
-  tabBarInfoContainer: {
-    position: "absolute",
-    bottom: 0,
-    left: 0,
-    right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: "black",
-        shadowOffset: { width: 0, height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3
-      },
-      android: {
-        elevation: 20
-      }
-    }),
-    alignItems: "center",
-    backgroundColor: "#fbfbfb",
-    paddingVertical: 20
-  },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: "rgba(96,100,109, 1)",
-    textAlign: "center"
-  },
-  navigationFilename: {
-    marginTop: 5
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: "center"
-  },
-  helpLink: {
-    paddingVertical: 15
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: "#2e78b7"
-  }
-});
 
 CustomersScreen.navigationOptions = {
-  title: "All Transaction",
+  title: "Customers",
   headerTintColor: "#fff",
   headerStyle: {
     backgroundColor: "#1287A5",
     fontSize: 40
   }
 };
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff"
+  }
+});
